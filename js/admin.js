12933cd370ba36c05c4e25d5fc8156d0333ae51c
Matt Brooks <mbrooks34@gsu.edu>
2013-10-04 16:22:50 -0400

    Added functionality for removing classes.

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,31 @@
-//On document load.
+/*
+	On document load.
+*/
 jQuery(function($) {
 	if($('#classDate').length) {
 		$('#classDate').datepicker({
 			dateFormat : 'm/d/yy'
 		});
 	}
+	
+	//Stops the delete links on the upcoming classes page from firing.
+	$('.removeLink').each(function(){
+		$(this).click(function(e) {
+			e.preventDefault();
+		});
+	});
 });
+
+
+/*
+	Functions
+*/
+//If this is needed.
+//var $j = jQuery.noConflict();
+
+function removeClass(url) {
+	var check = confirm("Are you sure you want to remove this class?");
+
+	if(check)
+		window.location.href = url;
+}
